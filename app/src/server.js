eb91c174c6f8619be1a77be0269626be1d8e76cc
minhnhatle104 <lnminh104@gmail.com>
2023-02-07 22:43:45 +0700

    Init connect between client and server

--- app/src/server.js
+++ app/src/server.js
@@ -2,11 +2,23 @@ const express = require('express')
 const app = express()
 const path = require("path")
 const http = require("http")
+const socketio = require('socket.io')
 
 const publicPathDirectory = path.join(__dirname,"../public");
 app.use(express.static(publicPathDirectory))
 
 const server = http.createServer(app)
+const io = socketio(server)
+
+// Khởi tạo kết nối server với client
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 
 const port = 3000
 
