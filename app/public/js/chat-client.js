393d8c509fcdc0cd2e47a7c4c3422773d7fefec5
minhnhatle104 <lnminh104@gmail.com>
2023-02-08 00:06:09 +0700

    Valid bad word

--- app/public/js/chat-client.js
+++ app/public/js/chat-client.js
@@ -3,7 +3,14 @@ var socket = io();
 document.getElementById("form-messages").addEventListener("submit",(e)=>{
     e.preventDefault()
     const messageText = document.getElementById("input-messages").value
-    socket.emit("Send message from client to server",messageText)
+    const acknowledgements = (errors)=>{
+        if(errors){
+            alert("Text sent invalid content!")
+        }else{
+            console.log("Send text successfully")
+        }
+    }
+    socket.emit("Send message from client to server",messageText,acknowledgements)
 })
 
 socket.on("Send message from server to client",(messageText)=>{
--- app/src/server.js
+++ app/src/server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require("path")
 const http = require("http")
 const socketio = require('socket.io')
+const Filter = require("bad-words")
 
 const publicPathDirectory = path.join(__dirname,"../public");
 app.use(express.static(publicPathDirectory))
@@ -13,8 +14,13 @@ const io = socketio(server)
 // Khởi tạo kết nối server với client
 io.on('connection', (socket) => {
   
-  socket.on("Send message from client to server",(messageText)=>{
+  socket.on("Send message from client to server",(messageText,callback)=>{
+    const filter = new Filter()
+    if(filter.isProfane(messageText)){
+      return callback("Message is not suitable")
+    }
     io.emit("Send message from server to client",messageText)
+    callback()
   })
 
   socket.on('disconnect', () => {
