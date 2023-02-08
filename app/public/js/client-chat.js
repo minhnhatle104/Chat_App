465cdc1a8e831473598b12e9d1ca50908cfb983e
minhnhatle104 <lnminh104@gmail.com>
2023-02-08 23:19:45 +0700

    Make message appear on UI chat

--- app/public/js/client-chat.js
+++ app/public/js/client-chat.js
@@ -1,51 +1,69 @@
 var socket = io();
 
-document.getElementById("form-messages").addEventListener("submit",(e)=>{
+document.getElementById("form-messages").addEventListener("submit", (e) => {
     e.preventDefault()
     const messageText = document.getElementById("input-messages").value
-    const acknowledgements = (errors)=>{
-        if(errors){
+    const acknowledgements = (errors) => {
+        if (errors) {
             alert("Text sent invalid content!")
-        }else{
+        } else {
             console.log("Send text successfully")
         }
     }
-    socket.emit("Send message from client to server",messageText,acknowledgements)
+    socket.emit("Send message from client to server", messageText, acknowledgements)
 })
 
-document.getElementById("btn-share-location").addEventListener("click",(e)=>{
+document.getElementById("btn-share-location").addEventListener("click", (e) => {
     e.preventDefault()
-    if(!navigator.geolocation){
+    if (!navigator.geolocation) {
         return alert("This browser doesn't support share location")
     }
-    navigator.geolocation.getCurrentPosition((position)=>{
-        const {latitude,longitude} = position.coords
-        socket.emit("Share location from client to server",{latitude,longitude})
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        socket.emit("Share location from client to server", { latitude, longitude })
     })
 })
 
-socket.on("Send message from server to client",(messageText)=>{
-    console.log("Client: ",messageText)
+socket.on("Send message from server to client", (message) => {
+    console.log("Client: ", message)
+    // Hiển thị giao diện lên màn hình
+    let messageHTML = `
+            <div class="message-item">
+            <div class="message__row1">
+            <p class="message__name">${username}</p>
+            <p class="message__date">${message.createAt}</p>
+            </div>
+            <div class="message__row2">
+            <p class="message__content">
+                ${message.messagesText}
+            </p>
+            </div>
+        </div>
+    `
+
+    document.getElementById("app__messages").innerHTML = messageHTML
 })
 
-socket.on("Share location from server to client",(linkLocation)=>{
-    console.log("Location: ",linkLocation)
+socket.on("Share location from server to client", (linkLocation) => {
+    console.log("Location: ", linkLocation)
 })
 
 // Xử lý query string
 const queryString = location.search
-const params = Qs.parse(queryString,{
-    ignoreQueryPrefix:true
+const params = Qs.parse(queryString, {
+    ignoreQueryPrefix: true
 })
-const {room,username} = params
+const { room, username } = params
 
-socket.emit("join room from client to server",{room,username})
+socket.emit("join room from client to server", { room, username })
+// hiển thị tên phòng lên giao diện
+document.getElementById("app__title").innerHTML = room
 
 // Nhận user list từ server
-socket.on("send user list from server to client",(userList)=>{
+socket.on("send user list from server to client", (userList) => {
     console.log(userList)
     let contentHTML = ""
-    userList.map((user)=>{
+    userList.map((user) => {
         contentHTML += `
             <li class="app__item-user">${user.username}</li>   
         `
